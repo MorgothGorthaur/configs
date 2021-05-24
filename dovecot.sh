349dbc9ba02c34c50f34d4177813ba2204049e73
MorgothGorthaur <58920821+MorgothGorthaur@users.noreply.github.com>
2021-05-24 08:06:28 +0300

    Update dovecot.sh

--- dovecot.sh
+++ dovecot.sh
@@ -14,13 +14,17 @@ echo " lda_mailbox_autocreate = yes " >>  /etc/dovecot/conf.d/15-lda.conf
 
 echo " 
 driver = mysql
-connect = host=localhost dbname=postfix user=postfix password=$mysql_password123
+connect = host=localhost dbname=postfix user=postfix password=$mysql_password123 " >> /etc/dovecot/dovecot-sql.conf.ext
+echo "
 default_pass_scheme = MD5-CRYPT
-password_query = SELECT password FROM mailbox WHERE username = '%u'
-user_query = SELECT maildir, 1024 AS uid, 1024 AS gid FROM mailbox WHERE username = '%u'
+password_query = SELECT password FROM mailbox WHERE username = '%u'  " >> /etc/dovecot/dovecot-sql.conf.ext
+
+echo "
+user_query = SELECT maildir, 1024 AS uid, 1024 AS gid FROM mailbox WHERE username = '%u' " >> /etc/dovecot/dovecot-sql.conf.ext
+echo "s
 user_query = SELECT CONCAT('/home/mail/',LCASE(`domain`),'/',LCASE(`maildir`)), 1024 AS uid, 1024 AS gid FROM mailbox WHERE username = '%u' " 
 >> /etc/dovecot/dovecot-sql.conf.ext
 
-cp ~/dovecot /etc/dovecot/dovecot.conf
+cp ~/dovecot.conf /etc/dovecot/dovecot.conf
 
 systemctl enable dovecot --now
