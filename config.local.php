4b05089477fcd3f5e15110638f55b5be463aa5ea
root <root@localhost.localdomain>
2021-05-24 03:55:33 +0300

    Committer: root <root@localhost.localdomain>  On branch main  Your branch is up to date with 'origin/main'. 	new file:   config.local.php

---
+++ config.local.php
@@ -0,0 +1,11 @@
+
+
+
+<?php
+
+$CONF['configured'] = true;
+$CONF['default_language'] = 'ru';
+$CONF['database_password'] = 'password123';
+$CONF['emailcheck_resolve_domain']='NO';
+$CONF['setup_password'] = '$2y$10$7DKVn.26xlFOuOqUDmQXF.UOHjh4EZHcgp7uaRS4tDr.obEqUwHB6';
+?>
