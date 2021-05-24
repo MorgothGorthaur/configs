c135539a452f04a44c164ffa1f2d58f812897195
MorgothGorthaur <58920821+MorgothGorthaur@users.noreply.github.com>
2021-05-24 15:22:59 +0300

    Update webmail.sh

--- webmail/webmail.sh
+++ webmail/webmail.sh
@@ -3,7 +3,7 @@ mkdir /usr/share/nginx/html/webmail
 tar -C /usr/share/nginx/html/webmail -xvf ~/tmp/roundcubemail-*.tar.gz --strip-components=1
 cp /usr/share/nginx/html/webmail/config/config.inc.php.sample /usr/share/nginx/html/webmail/config/config.inc.php
 
-cp ~/config.inc.php /usr/share/nginx/html/webmail/config/config.inc.php
+cp ~/configs/webmail/files/config.inc.php /usr/share/nginx/html/webmail/config/config.inc.php
 
 chown -R apache:apache /usr/share/nginx/html/webmail
 
@@ -13,7 +13,7 @@ GRANT ALL PRIVILEGES ON roundcubemail.* TO roundcube@localhost IDENTIFIED BY 'ro
 EOF
 mysql -uroot -p roundcubemail < /usr/share/nginx/html/webmail/SQL/mysql.initial.sql
 
-cp ~/php.ini /etc/php.ini
+cp ~/configs/webmail/files/php.ini /etc/php.ini
 
 systemctl restart php-fpm
 systemctl restart nginx
