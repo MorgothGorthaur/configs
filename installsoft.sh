c8419d6a2df98729ef967af66303fa89e2cec8cd
MorgothGorthaur <58920821+MorgothGorthaur@users.noreply.github.com>
2021-05-27 05:24:27 +0300

    Update installsoft.sh

--- installsoft.sh
+++ installsoft.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
-#dnf install chrony wget -y 
-#dnf update -y 
-#dnf install nginx -y 
-#dnf install epel-release -y 
-#dnf install http://rpms.remirepo.net/enterprise/remi-release-8.rpm -y 
-#dnf module install php:remi-7.2 -y 
-#dnf install php-fpm -y  
-#dnf install mariadb-server -y 
-#dnf install php-mysqli php-mbstring php-imap -y 
-#dnf install tar -y 
-#dnf install postfix postfix-mysql -y 
-#dnf install php-mysqli php-mbstring php-imap -y 
-#dnf install tar -y 
-#dnf install dovecot dovecot-mysql -y
-#dnf install php-pear php-mcrypt php-intl php-ldap php-pear-Net-SMTP -y 
+#dnf install chrony wget -y ;
+#dnf update -y ;
+#dnf install nginx -y ;
+#dnf install epel-release -y ;
+#dnf install http://rpms.remirepo.net/enterprise/remi-release-8.rpm -y ;
+#dnf module install php:remi-7.2 -y ;
+#dnf install php-fpm -y  ;
+#dnf install mariadb-server -y ;
+#dnf install php-mysqli php-mbstring php-imap -y ;
+#dnf install tar -y ;
+#dnf install postfix postfix-mysql -y ;
+#dnf install php-mysqli php-mbstring php-imap -y ;
+#dnf install tar -y ;
+#dnf install dovecot dovecot-mysql -y;
+#dnf install php-pear php-mcrypt php-intl php-ldap php-pear-Net-SMTP -y ;
 mkdir ~/tmp;
-wget https://sourceforge.net/projects/postfixadmin/files/latest/download -O ~/tmp/postfixadmin.tar.gz 
-wget https://github.com/roundcube/roundcubemail/releases/download/1.2.11/roundcubemail-1.2.11-complete.tar.gz -O ~/tmp/roundcubemail-1.2.11-complete.tar.gz
+wget https://sourceforge.net/projects/postfixadmin/files/latest/download -O ~/tmp/postfixadmin.tar.gz ;
+wget https://github.com/roundcube/roundcubemail/releases/download/1.2.11/roundcubemail-1.2.11-complete.tar.gz -O ~/tmp/roundcubemail-1.2.11-complete.tar.gz;
