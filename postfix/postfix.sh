9fa995dd730283985d5c727ea218a7f60f423ad1
MorgothGorthaur <58920821+MorgothGorthaur@users.noreply.github.com>
2021-05-27 07:00:29 +0300

    Update postfix.sh

--- postfix/postfix.sh
+++ postfix/postfix.sh
@@ -25,22 +25,22 @@ password = $mysql_password
 hosts = localhost
 dbname = postfix
 query = SELECT CONCAT(domain,'/',maildir) FROM mailbox WHERE username='%s' AND active = '1' " >>  /etc/postfix/mysql_virtual_mailbox_maps.cf
-echo "
-submission   inet  n  -  n  -  -  smtpd
-  -o smtpd_tls_security_level=may
-  -o smtpd_sasl_auth_enable=yes
-  -o smtpd_sasl_type=dovecot
-  -o smtpd_sasl_path=/var/spool/postfix/private/auth
-  -o smtpd_sasl_security_options=noanonymous
-  -o smtpd_sasl_local_domain=$myhostname
-smtps   inet  n  -  n  -  -  smtpd
-  -o syslog_name=postfix/smtps
-  -o smtpd_tls_wrappermode=yes
-  -o smtpd_sasl_auth_enable=yes
-  -o smtpd_client_restrictions=permit_sasl_authenticated,reject
-dovecot   unix  -  n  n  -  -  pipe
-  flags=DRhu user=vmail:vmail argv=/usr/libexec/dovecot/deliver -d ${recipient} " >> /etc/postfix/master.cf
-
+#echo "
+#submission   inet  n  -  n  -  -  smtpd
+#  -o smtpd_tls_security_level=may
+#  -o smtpd_sasl_auth_enable=yes
+#  -o smtpd_sasl_type=dovecot
+#  -o smtpd_sasl_path=/var/spool/postfix/private/auth
+#  -o smtpd_sasl_security_options=noanonymous
+#  -o smtpd_sasl_local_domain=$myhostname
+#smtps   inet  n  -  n  -  -  smtpd
+#  -o syslog_name=postfix/smtps
+#  -o smtpd_tls_wrappermode=yes
+#  -o smtpd_sasl_auth_enable=yes
+#  -o smtpd_client_restrictions=permit_sasl_authenticated,reject
+#dovecot   unix  -  n  n  -  -  pipe
+#  flags=DRhu user=vmail:vmail argv=/usr/libexec/dovecot/deliver -d ${recipient} " >> /etc/postfix/master.cf
+cat  ~/configs/postfix/files/master.cf >> /etc/postfix/master.cf
 
 
 systemctl enable postfix --now
