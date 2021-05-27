043d8d79c11916319a7bcc6d45787c32651dc9fe
MorgothGorthaur <58920821+MorgothGorthaur@users.noreply.github.com>
2021-05-27 19:00:57 +0300

    Update dovecot.sh

--- dovecot/dovecot.sh
+++ dovecot/dovecot.sh
@@ -6,7 +6,7 @@ cp ~/configs/dovecot/files/10-master.conf /etc/dovecot/conf.d/10-master.conf
 cp ~/configs/dovecot/files/10-auth.conf /etc/dovecot/conf.d/10-auth.conf
 cp ~/configs/dovecot/files/10-ssl.conf /etc/dovecot/conf.d/10-ssl.conf
 cp ~/configs/dovecot/files/dovecot.conf /etc/dovecot/dovecot.conf
-echo " lda_mailbox_autocreate = yes " >>  /etc/dovecot/conf.d/15-lda.conf
+#echo " lda_mailbox_autocreate = yes " >>  /etc/dovecot/conf.d/15-lda.conf
 echo '
 driver = mysql
 connect = host=localhost dbname=postfix user=postfix password=$mysql_password 
